ecd05cfccb8fa547a80726f53ba31fc2344a940f
EricMorata <ericoensis@gmail.com>
2022-10-20 14:22:25 +0200

    aplicando css

--- 03_validaciónFormularios/usuarios/index.php
+++ 03_validaciónFormularios/usuarios/index.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" type="text/css" href="./styleUsuarios.css" />
+    <link rel="stylesheet" type="text/css" href="./stylesUsuarios.css" />
     <title>Usuarios</title>
 </head>
 
@@ -159,9 +159,9 @@
         </span>
 
         <br><br>
-
+    <div id="boton">
         <input type="submit" value="Enviar">
-
+        </div>
     </form>
 
 </body>
