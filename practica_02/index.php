5dcc48bd82a02b5ec02c57c53bee38b73ff93128
EricMorata <ericoensis@gmail.com>
2022-10-21 11:15:58 +0200

    commit finalización de la clase para guardar cambios

--- practica_02/index.php
+++ practica_02/index.php
@@ -5,27 +5,88 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    
+
     <title>Practica 2</title>
 </head>
 
 <body>
     <h1>Práctica 2</h1>
-<?php
-if($_SERVER["REQUEST_METHOD"]=="POST"){
-    
-}
+    <?php
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $temp_dni = depurar($_POST["dni"]);
+        $temp_nombre = depurar($_POST["Nombre"]);
+        $temp_primerApellido = depurar($_POST["primerApellido"]);
+        $temp_segundoApellido = depurar($_POST["segundoApellido"]);
+        $temp_edad = depurar($_POST["edad"]);
+        $temp_email = depurar($_POST["email"]);
 
 
 
-function depurar($dato)
+        if (empty($temp_dni)) {
+            $error_dni = "El DNI es obligatorio";
+        } else {
+            $pattern = "/^[0-9]{8}[a-zA-Z]$/";
+            if (!preg_match($pattern, $temp_dni)) {
+                $error_dni = "El dni debe de tener 8 dígitos y un carácter";
+            } else {
+                $dni = $temp_dni;
+            }
+        }
+
+        if (empty($temp_nombre)) {
+            $error_nombre = "El nombre es obligatorio";
+        } else {
+            ucwords(strtolower($temp_nombre));
+            $pattern = "/^[a-zA-Z áéíóúÁÉÍÓÚñÑ]+$/";
+
+            if (!preg_match($pattern, $temp_nombre)) {
+                $error_nombre = "El nombre solo puede contener letras";
+            } else {
+                $nombre = $temp_nombre;
+                echo "<p>$nombre</p>";
+            }
+        }
+
+        if (empty($temp_primerApellido)) {
+            $error_primerApellido = "El nombre es obligatorio";
+        } else {
+            ucwords(strtolower($temp_primerApellido));
+            $pattern = "/^[a-zA-Z áéíóúÁÉÍÓÚñÑ]+$/";
+
+            if (!preg_match($pattern, $temp_primerApellido)) {
+                $error_primerApellido = "El nombre solo puede contener letras";
+            } else {
+                $primerApellido = $temp_primerApellido;
+                echo "<p>$primerApellido</p>";
+            }
+        }
+
+        if (empty($temp_segundoApellido)) {
+            $error_segundoApellido = "El nombre es obligatorio";
+        } else {
+            ucwords(strtolower($temp_segundoApellido));
+            $pattern = "/^[a-zA-Z áéíóúÁÉÍÓÚñÑ]+$/";
+
+            if (!preg_match($pattern, $temp_segundoApellido)) {
+                $error_segundoApellido = "El nombre solo puede contener letras";
+            } else {
+                $segundoApellidos = $temp_segundoApellido;
+                echo "<p>$segundoApellidos</p>";
+            }
+        }
+    }
+
+
+
+
+    function depurar($dato)
     {
         $dato = trim($dato);
         $dato = stripslashes($dato);
         $dato = htmlspecialchars($dato);
         return $dato;
     }
-?>
+    ?>
     <p>
         Crea un formulario con los siguientes campos: DNI, Nombre,
         Primer apellido, Segundo apellido, Edad y Correo electrónico. Valida el formulario mediante PHP.
@@ -53,21 +114,51 @@ function depurar($dato)
 
         <label for="dni">DNI:</label><br>
         <input type="text" name="dni" placeholder="8 carácteres y una letra"><br><br>
+        <span class="error">
+            *<?php
+                if (isset($error_dni)) echo $error_dni
+                ?>
+        </span><br>
 
         <label for="nombre">Nombre:</label><br>
         <input type="text" name="nombre" placeholder="Nombre completo"><br><br>
+        <span class="error">
+            *<?php
+                if (isset($error_nombre)) echo $error_nombre
+                ?>
+        </span>
 
         <label for="primerApellido">Primer apellido:</label><br>
         <input type="text" name="primerApellido" placeholder="Primer apellido"><br><br>
+        <span class="error">
+            *<?php
+                if (isset($error_primerApellido)) echo $error_primerApellido
+                ?>
+        </span>
 
         <label for="segundoApellido">Segundo apellido:</label><br>
         <input type="text" name="segundoApellido" placeholder="Segundo apellido"><br><br>
+        <span class="error">
+            *<?php
+                if (isset($error_segundoApellido)) echo $error_segundoApellido
+                ?>
+        </span>
 
         <label for="edad">Edad:</label><br>
         <input type="text" name="edad" placeholder="Indicame tu edad en Números"><br><br>
+        <span class="error">
+            *<?php
+                if (isset($error_edad)) echo $error_edad
+                ?>
+        </span>
 
         <label for="email">Email:</label><br>
         <input type="text" name="email" placeholder="example@email.com"><br><br>
+        <span class="error">
+            *<?php
+                if (isset($error_email)) echo $error_email
+                ?>
+        </span>
 
         <input type="submit" value="Enviar">
 
