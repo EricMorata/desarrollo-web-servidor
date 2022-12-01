3a06a073eb8095982f169a26003364cb50f5078e
EricMorata <ericoensis@gmail.com>
2022-12-01 12:59:31 +0100

    creacion ikea

---
+++ 08_laravel/ikea/resources/views/categorias/index.blade.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    <title>Index de categorias</title>
+</head>
+
+<body>
+    <h1>Index de categorias</h1>
+
+    <ul>
+        @foreach ($categorias as $categoria)
+
+        <li>  {{ $categoria -> nombre}} </li>
+<ul>
+    @php
+        $productos = $categoria -> productos //busca en la base de datos todos los productos que estan relacionados con la categoria
+    @endphp
+    @foreach ($productos as $producto)
+
+        <li> {{$producto -> nombre}} </li>
+
+    @endforeach
+</ul>
+
+        @endforeach
+    </ul>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+</body>
+
+</html>
