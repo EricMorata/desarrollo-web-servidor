8f7289cc084ec0a9ae742e96d15f868f46298876
EricMorata <ericoensis@gmail.com>
2022-12-01 12:58:40 +0100

    style

--- practica_03/ejercicio_01.php
+++ practica_03/ejercicio_01.php
@@ -1,3 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+
+    <title>Document</title>
+</head>
+<body>
+<div class="container">
+    <div class="row">
+     <div class="col-9">
+
+   
 <?php
 $productos = array(
 
@@ -14,7 +30,7 @@ array_multisort($nombre, SORT_ASC, $productos);
 
 
 
-<table border>
+<table class="table table-striped">
     <tr>
         <th>Nombre: </th>
         <th>Precio: </th>
@@ -47,8 +63,17 @@ array_multisort($nombre, SORT_ASC, $productos);
     ?>
 
     <br>
-    <table border>
+    <table class="table table-striped">
     <tr>
         <td>Total procuctos: <?php echo  $total_productos; ?></td>
         <td>Total Precio: <?php echo  $total_precio."€"; ?> </td>
     </tr>
+    </div>  
+    </div>
+</div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+
+</body>
+</html>
+
